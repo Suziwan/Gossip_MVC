ab918df476b115cbd03effceb955e488fb5e2528
Suziwan <christe.suzanne@gmail.com>
2023-02-06 11:16:33 +0000

    First commit

---
+++ app.rb
@@ -0,0 +1,11 @@
+require 'bundler'
+Bundler.require
+
+$:.unshift File.expand_path("./../lib", __FILE__)
+require 'router'
+require 'controller'
+require 'gossip'
+require 'view'
+
+routeur = Router.new
+routeur.perform
